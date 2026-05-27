b2dae4fb26811602172b84bebeb479f14de4bb47
Christopher jansson <crikke95@gmail.com>
2026-05-27 23:16:07 +0200

    chore: remove unused deps.go blank-import workaround

--- cmd/ci/deps.go
+++
@@ -1,7 +0,0 @@
-// This file ensures cobra and its dependencies are included in go.mod and vendor.
-// The blank import can be removed once cobra is imported in actual code.
-package main
-
-import (
-	_ "github.com/spf13/cobra"
-)
